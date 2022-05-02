6be803e9ab1c130e29510e3fdf16c17d97d0252b
alepase2005 <alessandropaselli00@gmail.com>
2022-05-02 21:15:25 +0200

    initial commit

--- src/powers/Powers.java
+++ src/powers/Powers.java
@@ -18,6 +18,9 @@ public class Powers {
     public static void main(String[] args) {
         Scanner scan= new Scanner(System.in);
         int ans=0;
+        double ans1=1;
+        double ans2=1;
+        double ans3=1;
         while(ans!=4){
          System.out.println("Please, select an option");
          System.out.println("1… Find the value of a number squared");
@@ -28,18 +31,27 @@ public class Powers {
          if(ans==1){
              System.out.println("Please, enter a double");
              double n1=scan.nextDouble();
-             System.out.println(Math.pow(n1,2));
+             for(int y=0;y<2;y++){
+                ans1=ans1*n1; 
+             }
+             System.out.println(ans1);
          }
          else if(ans==2){
              System.out.println("Please, enter a double");
              double n2=scan.nextDouble();
-             System.out.println(Math.pow(n2,3)); 
+             for(int y=0;y<3;y++){
+                ans2=ans2*n2; 
+             }
+             System.out.println(ans2); 
          }
          else if(ans==3){
              System.out.println("Please, enter a double and an exponent");
              double n3=scan.nextDouble();
              double ex=scan.nextDouble();
-             System.out.println(Math.pow(n3,ex)); 
+             for(int y=0;y<ex;y++){
+                ans3=ans3*n3; 
+             }
+             System.out.println(ans3); 
          }
         }
         System.out.println("Thank you for using this program");
